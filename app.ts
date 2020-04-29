3719db80b27e436e00fe4f93cbe5126d071b922f
Julia Lojek <julialojek@gmail.com>
2020-04-29 18:59:15 +0200

    core types

--- app.ts
+++ app.ts
@@ -1,1 +1,14 @@
-console.log("THE CODE:");
+function add(n1: number, n2: number, printResult: boolean, phrase: string) {
+  if (printResult) {
+    console.log(phrase + (n1 + n2));
+  } else {
+    return n1 + n2;
+  }
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const phrase = "the result is: ";
+
+add(number1, number2, printResult, phrase);
