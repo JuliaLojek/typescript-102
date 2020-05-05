317dc49a9cd97978160c4c10c69ff0db6ce71b55
Julia Lojek <julialojek@gmail.com>
2020-05-05 18:23:21 +0200

    getter and setter methods

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,22 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Department = /** @class */ (function () {
     function Department(id, name) {
         this.id = id;
         this.name = name;
+        // private readonly id: string;
         // private name: string;
         this.employees = [];
         // shorthand for creating properties, public keyword here is necessary to create a field inside the class
@@ -21,9 +35,62 @@ var Department = /** @class */ (function () {
     };
     return Department;
 }());
-var accounting = new Department("101", "Accounting");
+var ITDepartment = /** @class */ (function (_super) {
+    __extends(ITDepartment, _super);
+    function ITDepartment(id, admins) {
+        var _this = _super.call(this, id, "IT") || this;
+        _this.admins = admins;
+        _this.admins = admins;
+        return _this;
+    }
+    return ITDepartment;
+}(Department));
+var AccountingDepartment = /** @class */ (function (_super) {
+    __extends(AccountingDepartment, _super);
+    function AccountingDepartment(id, reports) {
+        var _this = _super.call(this, id, "Accounting") || this;
+        _this.reports = reports;
+        _this.reports = reports;
+        _this.lastReport = reports[0];
+        return _this;
+    }
+    Object.defineProperty(AccountingDepartment.prototype, "mostRecentReport", {
+        get: function () {
+            if (this.lastReport) {
+                return this.lastReport;
+            }
+            throw new Error("No report found");
+        },
+        set: function (value) {
+            this.addReport(value);
+        },
+        enumerable: true,
+        configurable: true
+    });
+    AccountingDepartment.prototype.addEmployee = function (name) {
+        if (name === "Max") {
+            return;
+        }
+        this.employees.push(name);
+    };
+    AccountingDepartment.prototype.addReport = function (text) {
+        this.reports.push(text);
+        this.lastReport = text;
+    };
+    AccountingDepartment.prototype.printReports = function () {
+        console.log(this.reports);
+    };
+    return AccountingDepartment;
+}(Department));
+var accounting = new AccountingDepartment("101", []);
+accounting.addReport("Something went wrong");
+console.log(accounting.mostRecentReport); //  accessing a private field through getter method
+accounting.mostRecentReport = "2020 report"; //  setting a private field through setter method
+var it = new ITDepartment("201", ["Kevin", "Mark", "Daisy"]);
 accounting.describe();
 accounting.addEmployee("Max");
 accounting.addEmployee("Julia");
 accounting.printEmployeeInformation();
+accounting.printReports();
+it.describe();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 class Department {
+  // private readonly id: string;
   // private name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
-  constructor(private id: string, public name: string) {
+  constructor(private readonly id: string, public name: string) {
     // shorthand for creating properties, public keyword here is necessary to create a field inside the class
     // this.name = name;
   }
@@ -11,20 +12,66 @@ class Department {
     // "this" has to be an instance of the Department class
     console.log("Department #" + this.id + ": " + this.name);
   }
-
   addEmployee(emplyee: string) {
     this.employees.push(emplyee);
   }
-
   printEmployeeInformation() {
     console.log(this.employees.length);
     console.log(this.employees);
   }
 }
 
-const accounting = new Department("101", "Accounting");
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+}
+
+class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {   // getter method - access to private values
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found");
+  }
+
+  set mostRecentReport(value: string) {  // setter method
+    this.addReport(value);
+  }
+
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+    this.reports = reports;
+    this.lastReport = reports[0];
+  }
+  addEmployee(name: string) {
+    if (name === "Max") {
+      return;
+    }
+    this.employees.push(name);
+  }
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const accounting = new AccountingDepartment("101", []);
+accounting.addReport("Something went wrong");
+console.log(accounting.mostRecentReport);     //  accessing a private field through getter method
+accounting.mostRecentReport = "2020 report";  //  setting a private field through setter method
+const it = new ITDepartment("201", ["Kevin", "Mark", "Daisy"]);
 
 accounting.describe();
 accounting.addEmployee("Max");
 accounting.addEmployee("Julia");
 accounting.printEmployeeInformation();
+accounting.printReports();
+
+it.describe();
