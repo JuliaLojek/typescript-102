a5da189e7d956756555bc0c4db8e933152f9a8bd
Julia Lojek <julialojek@gmail.com>
2020-05-05 17:38:11 +0200

    classes: private, public

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,29 @@
 "use strict";
 var Department = /** @class */ (function () {
-    function Department(n) {
-        this.name = n;
+    function Department(id, name) {
+        this.id = id;
+        this.name = name;
+        // private name: string;
+        this.employees = [];
+        // shorthand for creating properties, public keyword here is necessary to create a field inside the class
+        // this.name = name;
     }
     Department.prototype.describe = function () {
-        console.log("Department: " + this.name);
+        // "this" has to be an instance of the Department class
+        console.log("Department #" + this.id + ": " + this.name);
+    };
+    Department.prototype.addEmployee = function (emplyee) {
+        this.employees.push(emplyee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
     };
     return Department;
 }());
-var accounting = new Department("Accounting");
+var accounting = new Department("101", "Accounting");
 accounting.describe();
+accounting.addEmployee("Max");
+accounting.addEmployee("Julia");
+accounting.printEmployeeInformation();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,30 @@
 class Department {
-  name: string;
+  // private name: string;
+  private employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private id: string, public name: string) {
+    // shorthand for creating properties, public keyword here is necessary to create a field inside the class
+    // this.name = name;
   }
 
-  describe(this: Department) {  // "this" has to be an instance of the Department class
-    console.log("Department: " + this.name);
+  describe(this: Department) {
+    // "this" has to be an instance of the Department class
+    console.log("Department #" + this.id + ": " + this.name);
+  }
+
+  addEmployee(emplyee: string) {
+    this.employees.push(emplyee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const accounting = new Department("Accounting");
+const accounting = new Department("101", "Accounting");
 
 accounting.describe();
+accounting.addEmployee("Max");
+accounting.addEmployee("Julia");
+accounting.printEmployeeInformation();
