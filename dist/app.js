46cab6ed53680ebc14b82f0a85281c6da71b21af
Julia Lojek <julialojek@gmail.com>
2020-05-13 13:06:07 +0200

    generic functions

--- dist/app.js
+++ dist/app.js
@@ -8,4 +8,18 @@ const promise = new Promise((resolve, reject) => {
         resolve("This is resolved!");
     }, 2000);
 });
+///////// generci functions
+function merge(objA, objB) {
+    // if we combine objects like that we then couldn't access properties or methods of a merged objects (the result of the function) because it only knows that it's an object but doesn't know it's structure
+    return Object.assign(objA, objB);
+}
+// const mergedObj = merge({name: "julia"}, {age: 26});
+// const age = mergedObj.age;    // it's impossible here
+// this is why we use generics:
+function merge2(objA, objB) {
+    return Object.assign(objA, objB);
+}
+const mergedObj2 = merge2({ name: "julia" }, { age: 26 });
+const age2 = mergedObj2.age; // now it works
+console.log(age2);
 //# sourceMappingURL=app.js.map
